122190e4886d418f0dd93870e034d2938364316c
varunramesh26 <varunramesh.07@gmail.com>
2022-06-06 18:06:37 +0530

    base django models

---
+++ accounts/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+from django.conf import settings
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, **extra_fields):
+        """ Create and save a user with the given email, and password. """
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.save()
+        return user
+
+
+    def create_user(self, email, username, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.save()
+        return user
+
+    def create_superuser(self, email, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Staff user must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Super user must have is_superuser=True')
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Super user must have is_admin=True')
+        print("SUPERUSER")
+        print(extra_fields)
+        return self._create_user(email, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField()
+    email = models.EmailField(max_length=255, unique=True)
+    # username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True)
+    is_active = models.BooleanField(default=False, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=50)
+    role = models.UUIDField()
+    profile_picture = models.CharField(500)
+    status = models.CharField(max_length=50)
+    subscription = models.CharField(50)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def get_full_name(self):
+        return f"{self.first_name} - {self.last_name}"
+
+    # def get_username(self):
+    #     return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def __str__(self):
+        return self.email
+
+
+class UserRole(models.Model):
+    id = models.UUIDField()
+    role_name = models.CharField(User, max_length=255)
---
+++ datahub/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+
+class Organization(models.Model):
+    """ Organization model """
+    IS_ACTIVE = 1
+    IS_INACTIVE = 0
+    STATUSES = [(IS_ACTIVE, 'is_active'),
+            (IS_INACTIVE, 'is_inactive')
+        ]
+
+    id = models.UUIDField()
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    address = models.JSONField()
+    phone_number = models.CharField()
+    logo = models.CharField(max_length=500, null=True, blank=True)
+    hero_image = models.CharField(max_length=500, null=True, blank=True)
+    website = models.CharField(max_length=255)
+    status = models.CharField(max_length=10,
+                            choices=STATUSES,
+                            default=IS_ACTIVE)
+
+
+class UserOrganizationMap(models.Model):
+    """ UserOrganizationMap model """
+    id = models.UUIDField()
+    user_id = models.UUIDField(settings.AUTH_USER_MODEL)
+    organization_id = models.UUIDField(Organization)
+
