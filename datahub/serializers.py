3eab00cca50f4ff3b4d6a4e77d202667fd31f299
ugeshdg <ugesh@digitalgreen.org>
2022-07-21 11:08:39 +0530

    datasest serializer updated

--- datahub/serializers.py
+++ datahub/serializers.py
@@ -208,7 +208,24 @@ class DatasetSerializer(serializers.ModelSerializer):
         """_summary_"""
 
         model = Datasets
-        fields = Constants.ALL
+        fields = [
+            "user_map",
+            "name",
+            "description",
+            "category",
+            "geography",
+            "crop_detail",
+            "constantly_update",
+            "dataset_size",
+            "connector_availability",
+            "age_of_date",
+            "sample_dataset",
+            "data_capture_start",
+            "data_capture_end",
+            "remarks",
+            "is_enabled",
+            "approval_status",
+        ]
 
 
 class DatahubDatasetsDetailSerializer(serializers.ModelSerializer):
