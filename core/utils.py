a0abfd66535037919f77c728c89d69099671f24c
varunramesh26 <varunramesh.07@gmail.com>
2022-08-04 21:52:52 +0530

    minor fix for localhost urls

--- core/utils.py
+++ core/utils.py
@@ -70,8 +70,8 @@ def replace_query_param(url, key, val, req):
     """
     (scheme, netloc, path, query, fragment) = parse.urlsplit(str(url))
     netloc = req.META.get("HTTP_HOST")
-    scheme = "http" if "localhost" in netloc else "https"  # type: ignore
-    path = path if "localhost" in netloc else "/be" + path  # type: ignore
+    scheme = "http" if "localhost" or ("127.0.0.1" in netloc) else "https"  # type: ignore
+    path = path if "localhost" or ("127.0.0.1" in netloc) else "/be" + path  # type: ignore
     query_dict = parse.parse_qs(query, keep_blank_values=True)
     query_dict[str(key)] = [str(val)]
     query = parse.urlencode(sorted(list(query_dict.items())), doseq=True)
@@ -85,8 +85,8 @@ def remove_query_param(url, key, req):
     """
     (scheme, netloc, path, query, fragment) = parse.urlsplit(str(url))
     netloc = req.META.get("HTTP_HOST")
-    scheme = "http" if "localhost" in netloc else "https"
-    path = path if "localhost" in netloc else "/be" + path
+    scheme = "http" if "localhost" or ("127.0.0.1" in netloc) else "https"  # type: ignore
+    path = path if "localhost" or ("127.0.0.1" in netloc) else "/be" + path  # type: ignore
     # netloc = "datahubtest.farmstack.co"
     query_dict = parse.parse_qs(query, keep_blank_values=True)
     query_dict.pop(key, None)
