ee13696c17f8b3f76e934cebd83b5020c603c6a4
varunramesh26 <varunramesh.07@gmail.com>
2022-08-29 18:44:24 +0530

    minor fix dept & project listing

--- participant/views.py
+++ participant/views.py
@@ -703,6 +703,7 @@ class ParticipantDepatrmentViewSet(GenericViewSet):
         data = (
             # Department.objects.filter(Q(status=True, **filters) | Q(department_name=Constants.DEFAULT))
             Department.objects.filter(status=True, **filters)
+            .exclude(department_name=Constants.DEFAULT)
             .order_by(Constants.UPDATED_AT)
             .reverse()
             .all()
@@ -791,6 +792,7 @@ class ParticipantProjectViewSet(GenericViewSet):
             Project.objects.select_related(Constants.DEPARTMENT_ORGANIZATION)
             # .filter(Q(status=True, **filters) | Q(project_name=Constants.DEFAULT))
             .filter(status=True, **filters)
+            .exclude(project_name=Constants.DEFAULT)
             .order_by(Constants.UPDATED_AT)
             .reverse()
             .all()
