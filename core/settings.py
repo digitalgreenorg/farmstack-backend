8c0bf879f9a4727bdbafcf2efba142ec245675e7
nsbits <nilanjan@digitalgreen.org>
2022-12-06 17:47:05 +0530

    json string was accessed as dictionary key value, changed to proper syntax

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ DATABASES = {
     # "default": {
     #     "ENGINE": "django.db.backends.sqlite3",
     #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    # },
 }
 # "default": {
 #     "ENGINE": "django.db.backends.sqlite3",
