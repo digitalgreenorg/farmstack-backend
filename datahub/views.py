724a1f09b393e4b900220b28010a093985ec3526
varunramesh26 <varunramesh.07@gmail.com>
2023-02-01 15:49:00 +0530

    fixed empty categories for datahub v1

--- datahub/views.py
+++ datahub/views.py
@@ -1311,8 +1311,11 @@ class DatahubDatasetsViewSet(GenericViewSet):
                 .all()
                 .distinct()
             )
-            with open(Constants.CATEGORIES_FILE, "r") as json_obj:
-                category_detail = json.loads(json_obj.read())
+            if os.path.exists(Constants.CATEGORIES_FILE):
+                with open(Constants.CATEGORIES_FILE, "r") as json_obj:
+                    category_detail = json.loads(json_obj.read())
+            else:
+                category_detail = []
         except Exception as error:  # type: ignore
             logging.error("Error while filtering the datasets. ERROR: %s", error)
             return Response(
