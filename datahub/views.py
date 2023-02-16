3c8473c4e4fa1ce416aac4fd7d0a98db30ae1e52
waseem <waseempasha@digitalgreen.org>
2023-02-16 15:10:21 +0530

    Revert "delete updated"
    This reverts commit b9c167bcc8f8748cd539567e5e33654f996aa087.

--- datahub/views.py
+++ datahub/views.py
@@ -525,11 +525,14 @@ class ParticipantViewSet(GenericViewSet):
             Constants.ORGANIZATION
         ).get(user_id=pk)
         organization = Organization.objects.get(id=user_organization.organization_id)
-        if participant.status:
+
+        if participant.status is not False and organization.status is not False:
             participant.status = False
+            organization.status = False
+
             try:
                 if participant.on_boarded_by:
-                    datahub_admin = participant.on_boarded_by
+                    datahub_admin = User.objects.filter(id=participant.on_boarded_by).first()
                 else:
                     datahub_admin = User.objects.filter(role_id=1).first()
                 admin_full_name = string_functions.get_full_name(
@@ -553,6 +556,7 @@ class ParticipantViewSet(GenericViewSet):
 
                 # delete data & trigger_email
                 self.perform_create(participant)
+                self.perform_create(organization)
                 email_render = render(
                     request,
                     Constants.DATAHUB_ADMIN_DELETES_PARTICIPANT_ORGANIZATION,
@@ -576,7 +580,7 @@ class ParticipantViewSet(GenericViewSet):
                     {"message": ["An error occured"]}, status=status.HTTP_200_OK
                 )
 
-        elif not participant.status:
+        elif participant.status is False and organization.status is False:
             return Response(
                 {"message": ["Object already deleted"]},
                 status=status.HTTP_204_NO_CONTENT,
