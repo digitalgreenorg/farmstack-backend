81188e347b55e8d6a6debeed3c65db8500dff721
waseem <waseempasha@digitalgreen.org>
2023-03-14 17:57:05 +0530

    Minor bug fix: moved configuration to condition key

--- connectors/views.py
+++ connectors/views.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 import pandas as pd
+from core import settings
+from core.constants import Constants
+from core.utils import CustomPagination
 from django.db import transaction
 from django.shortcuts import render
 from rest_framework import status
@@ -17,9 +20,6 @@ from connectors.serializers import (
     ConnectorsMapSerializer,
     ConnectorsSerializer,
 )
-from core import settings
-from core.constants import Constants
-from core.utils import CustomPagination
 
 # Create your views here.
 
@@ -29,7 +29,7 @@ class ConnectorsViewSet(GenericViewSet):
 
     queryset = Connectors.objects.all()
     pagination_class = CustomPagination
-    
+
     @transaction.atomic
     def create(self, request, *args, **kwargs):
         """POST method: create action to save an object by sending a POST request"""
@@ -56,14 +56,12 @@ class ConnectorsViewSet(GenericViewSet):
         instance = self.get_object()
         serializer = ConnectorsSerializer(instance=instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     @transaction.atomic
     def update(self, request, pk):
         """PUT method: update or send a PUT request on an object of the Product model"""
         instance = self.get_object()
-        connector_serializer = ConnectorsCreateSerializer(
-            instance, data=request.data, partial=True
-        )
+        connector_serializer = ConnectorsCreateSerializer(instance, data=request.data, partial=True)
         connector_serializer.is_valid(raise_exception=True)
         connector_serializer.save()
         for maps in request.data.get(Constants.MAPS, []):
@@ -72,7 +70,7 @@ class ConnectorsViewSet(GenericViewSet):
                 instance = ConnectorsMap.objects.get(id=maps.get(Constants.ID))
                 serializer = ConnectorsMapCreateSerializer(instance, data=maps, partial=True)
             else:
-               serializer = ConnectorsMapCreateSerializer(data=maps)
+                serializer = ConnectorsMapCreateSerializer(data=maps)
             serializer.is_valid(raise_exception=True)
             serializer.save()
         return Response(connector_serializer.data, status=status.HTTP_200_OK)
@@ -97,9 +95,9 @@ class ConnectorsViewSet(GenericViewSet):
         try:
             left_dataset_file_path = integrate1.get("left_dataset_file_path")
             right_dataset_file_path = integrate1.get("right_dataset_file_path")
-            left_columns = integrate1.get("left_selected")
-            right_columns = integrate1.get("right_selected") 
- 
+            left_columns = integrate1.get("condition").get("left_selected")
+            right_columns = integrate1.get("condition").get("right_selected")
+
             if left_dataset_file_path.endswith(".xlsx") or left_dataset_file_path.endswith(".xls"):
                 df1 = pd.read_excel(os.path.join(settings.MEDIA_ROOT, left_dataset_file_path), usecols=left_columns)
             else:
@@ -109,17 +107,33 @@ class ConnectorsViewSet(GenericViewSet):
             else:
                 df2 = pd.read_csv(os.path.join(settings.MEDIA_ROOT, right_dataset_file_path), usecols=right_columns)
             # Join the dataframes
-            result = pd.merge(df1, df2, how=integrate1.get("how", "left"), left_on=integrate1.get("left_on"), right_on=integrate1.get("right_on"))
+            result = pd.merge(
+                df1,
+                df2,
+                how=integrate1.get("condition").get("how", "left"),
+                left_on=integrate1.get("condition").get("left_on"),
+                right_on=integrate1.get("condition").get("right_on"),
+            )
             for i in range(1, len(data)):
                 integrate1 = data[i]
                 right_dataset_file_path = integrate1.get("right_dataset_file_path")
-                right_columns = integrate1.get("right_selected") 
+                right_columns = integrate1.get("condition").get("right_selected")
                 if right_dataset_file_path.endswith(".xlsx") or right_dataset_file_path.endswith(".xls"):
-                    df2 = pd.read_excel(os.path.join(settings.MEDIA_ROOT, right_dataset_file_path), usecols=right_columns)
+                    df2 = pd.read_excel(
+                        os.path.join(settings.MEDIA_ROOT, right_dataset_file_path), usecols=right_columns
+                    )
                 else:
-                    df2 = pd.read_csv(os.path.join(settings.MEDIA_ROOT, right_dataset_file_path), usecols=right_columns)
+                    df2 = pd.read_csv(
+                        os.path.join(settings.MEDIA_ROOT, right_dataset_file_path), usecols=right_columns
+                    )
                 # Join the dataframes
-                result = pd.merge(result, df2, how=integrate1.get("how", "left"), left_on=integrate1.get("left_on"), right_on=integrate1.get("right_on"))
+                result = pd.merge(
+                    result,
+                    df2,
+                    how=integrate1.get("how", "left"),
+                    left_on=integrate1.get("left_on"),
+                    right_on=integrate1.get("right_on"),
+                )
 
             return Response(result.to_json(orient="records"), status=status.HTTP_200_OK)
         except Exception as e:
