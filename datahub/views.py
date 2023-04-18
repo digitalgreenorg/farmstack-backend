c4fe2b00f0c30e6457adb8bcafca63971fd77be5
ugeshdg <ugesh@digitalgreen.org>
2023-04-18 13:26:14 +0530

    email invite uodated

--- datahub/views.py
+++ datahub/views.py
@@ -580,31 +580,31 @@ class MailInvitationViewSet(GenericViewSet):
         try:
             email_list = request.data.get("to_email")
             emails_found, emails_not_found = ([] for i in range(2))
+            # for email in email_list:
+            #     if User.objects.filter(email=email):
+            #         emails_found.append(email)
+            #     else:
+            #         emails_not_found.append(email)
+            user = User.objects.filter(role_id=1).first()
+            full_name = user.first_name + " " + str(user.last_name) if user.last_name else user.first_name
+            data = {
+                "datahub_name": os.environ.get("DATAHUB_NAME", "datahub_name"),
+                "participant_admin_name": full_name,
+                "datahub_site": os.environ.get("DATAHUB_SITE", "datahub_site"),
+            }
+            # render email from query_email template
             for email in email_list:
-                if User.objects.filter(email=email):
-                    emails_found.append(email)
-                else:
-                    emails_not_found.append(email)
-
-            for user in User.objects.filter(email__in=emails_found):
-                full_name = user.first_name + " " + str(user.last_name) if user.last_name else user.first_name
-                data = {
-                    "datahub_name": os.environ.get("DATAHUB_NAME", "datahub_name"),
-                    "participant_admin_name": full_name,
-                    "datahub_site": os.environ.get("DATAHUB_SITE", "datahub_site"),
-                }
-
-                # render email from query_email template
-                email_render = render(request, "datahub_admin_invites_participants.html", data)
-                mail_body = email_render.content.decode("utf-8")
-
-                Utils().send_email(
-                    to_email=[user.email],
-                    content=mail_body,
-                    subject=os.environ.get("DATAHUB_NAME", "datahub_name") + Constants.PARTICIPANT_INVITATION_SUBJECT,
-                )
-
-            failed = f"No participants found for emails: {emails_not_found}"
+                try:
+                    email_render = render(request, "datahub_admin_invites_participants.html", data)
+                    mail_body = email_render.content.decode("utf-8")
+                    Utils().send_email(
+                        to_email=[email],
+                        content=mail_body,
+                        subject=os.environ.get("DATAHUB_NAME", "datahub_name") + Constants.PARTICIPANT_INVITATION_SUBJECT,
+                    )
+                except Exception as e:
+                    emails_not_found.append()
+            failed = f"No able to send emails to this emails: {emails_not_found}"
             LOGGER.warning(failed)
             return Response(
                 {
