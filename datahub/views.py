d66fc245ef0647149cbb4e883de7e797999f8baf
ugeshdg <ugesh@digitalgreen.org>
2023-04-24 15:26:22 +0530

    file size updated

--- datahub/views.py
+++ datahub/views.py
@@ -2241,6 +2241,7 @@ class DatasetFileV2View(GenericViewSet):
         data = serializer.data
         instance = DatasetV2File.objects.get(id=data.get("id"))
         instance.standardised_file=instance.file # type: ignore
+        instance.file_size=os.path.getsize(os.path.join(settings.DATASET_FILES_URl, str(instance.file)))
         instance.save()
         LOGGER.info("Dataset created Successfully.")
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -2283,6 +2284,7 @@ class DatasetFileV2View(GenericViewSet):
         # data = request.data
         standardised_file_path = os.path.join(instance.dataset.name, instance.source, file_name)
         data["standardised_configuration"] = config
+        data["file_size"]=os.path.getsize(os.path.join(settings.DATASET_FILES_URl, str(standardised_file_path)))
         serializer = self.get_serializer(instance, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
