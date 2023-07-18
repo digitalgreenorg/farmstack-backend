fcf4354a95cfcb08215279b8958bfcaeecc0750e
ektakum18 <ekta@digitalgreen.org>
2023-07-18 14:45:18 +0530

    test_cases

--- datahub/tests/test_views_new.py
+++ datahub/tests/test_views_new.py
@@ -1,32 +1,3 @@
-from django.test import SimpleTestCase
-from django.urls import resolve, reverse
-from datahub.views import StandardisationTemplateView
-
-class TestUrls(SimpleTestCase):
-    # def test_org_invalid(self):
-    #     url = reverse('STANDARDISE-list')
-    #     self.assertNotEqual(resolve(url).func, StandardisationTemplateView)
-
-    # def test_org_valid_func(self):
-    #     url = reverse("STANDARDISE-list")
-    #     assert resolve(url)._func_path == "datahub.views.StandardisationTemplateView" # type: ignore
-        
-    def test_standardise_create_valid(self):
-        url = reverse("stardardise-list")
-
-        print(resolve(url))
-        print(resolve(url)._func_path) # type: ignore
-        assert resolve(url)._func_path == "datahub.views.StandardisationTemplateView" # type: ignore
-
-
-
-
-
-
-
-##########################
-# import uuid
-# from django.test import Client, TestCase
 import json
 from turtle import st
 from django.urls import reverse
