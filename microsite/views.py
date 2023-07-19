12dc8c1ce86c66ee1870aaaf257484e1e749c40d
Akshata Naik <akshatanaik@Akshatas-MacBook-Pro-2.local>
2023-07-19 11:44:48 +0530

    FSPROD-1782: Microsite Legal policies Test cases

--- microsite/views.py
+++ microsite/views.py
@@ -554,20 +554,10 @@ class PolicyAPIView(GenericViewSet):
     permission_classes = []
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        try:
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-            serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data)
-        except Exception as error:
-            LOGGER.error(error, exc_info=True)
-            return Response(str(error), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
