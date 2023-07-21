c2e803e7fe69c7ee400a3e1fdd01670a5457516a
Jai <jai@Jais-MacBook-Pro-2.local>
2023-07-21 10:58:19 +0530

    removed unecessary files

--- datahub/tests/tests_v2/test_urls.py
+++ datahub/tests/tests_v2/test_urls.py
@@ -1,11 +1,11 @@
-import pytest
-from django.test import SimpleTestCase
-from django.urls import resolve, reverse
-
-
-class TestUrls(SimpleTestCase):
-    def test_datasetv2_list(self):
-        """Unit test cases for Dataset V2 URLs"""
-        url = reverse("dataset/v2-list")
-        print("List View URL", url, resolve(url)._func_path)
-        assert resolve(url)._func_path == "datahub.views.DatasetV2ViewSet"
+# import pytest
+# from django.test import SimpleTestCase
+# from django.urls import resolve, reverse
+#
+#
+# class TestUrls(SimpleTestCase):
+#     def test_datasetv2_list(self):
+#         """Unit test cases for Dataset V2 URLs"""
+#         url = reverse("dataset/v2-list")
+#         print("List View URL", url, resolve(url)._func_path)
+#         assert resolve(url)._func_path == "datahub.views.DatasetV2ViewSet"
