1a72843975290212ed9383ebb0681c572a3e6b6c
ektakum18 <ekta@digitalgreen.org>
2023-08-04 15:19:53 +0530

    fsprod-1789 test_cases for dashboard

--- datahub/tests/test_dataset_views.py
+++ datahub/tests/test_dataset_views.py
@@ -1,7 +1,9 @@
+from unicodedata import category
 from rest_framework.reverse import reverse
 from django.test import Client, TestCase
 # from rest_framework import status
 import json
+from connectors.models import Connectors, ConnectorsMap
 from datahub.models import  DatasetV2,Organization, UsagePolicy, UserOrganizationMap,DatasetV2File
 from accounts.models import User, UserRole
 from datahub.views import UsagePolicyListCreateView
@@ -253,20 +255,153 @@ class TestCasesDatasets(TestCase):
         assert data['sent'][0]['organization_email'] == 'admin2_org@dg.org'
     
     #invalid case
-    def test_request_dataset_invalid(self):
-        """ invalid user map request-dataset """
-        file2_id= self.response2.json()['id']
-        UsagePolicy.objects.create(dataset_file_id= file2_id,
-                                    user_organization_map=self.admin_map2)
-        file3_id= self.response3.json()['id']
-        UsagePolicy.objects.create(dataset_file_id= file3_id,
-                                    user_organization_map=self.admin_map1)
-        data_user = {
-            "user_map": "123"
+    # def test_request_dataset_invalid(self):
+    #     """ invalid user map request-dataset """
+    #     file2_id= self.response2.json()['id']
+    #     UsagePolicy.objects.create(dataset_file_id= file2_id,
+    #                                 user_organization_map=self.admin_map2)
+    #     file3_id= self.response3.json()['id']
+    #     UsagePolicy.objects.create(dataset_file_id= file3_id,
+    #                                 user_organization_map=self.admin_map1)
+    #     data_user = {
+    #         "user_map": "123"
+    #     }
+    #     response = self.client_admin.post(self.dataset_request+"requested_datasets/", data_user)
+    #     data = response.json()
+    #     # print("***test_request_dataset_invalid***", response.status_code)
+    #     # print("***test_request_dataset_invalid***", data)
+    #     assert response.status_code == 500
+    #     assert data == f"Issue while Retrive requeted data ['“{data_user['user_map']}” is not a valid UUID.']"
+        
+        
+
+        
+first_datasets = {
+    "name": "dataset dash 1",
+    "description": "dataset dash desc 1",
+    "geography": "tpt 1",
+    "constantly_update": False,
+    "category": ({"category dash": ["dashboard category"]})
+}
+second_datasets = {
+    "name": "dataset dash 2",
+    "description": "dataset dash desc 2",
+    "geography": "tpt 2",
+    "constantly_update": False,
+}
+third_datasets = {
+    "name": "dataset dash 3",
+    "description": "dataset dash desc 3",
+    "geography": "tpt 3",
+    "constantly_update": False,
+}
+connectors_info ={
+    'name':'connector hub',
+    "description":'vjskmjks',
+}
+# auth = {
+#     "token": "null"
+# }
+# auth_co_steward = {
+#     "token": "null"
+# }
+# auth_participant= {
+#     "token": "null"
+# }
+
+class TestCasesDashboard(TestCase):
+    """test cases for dashboard """ 
+    @classmethod
+    def setUpClass(self) -> None:
+        super().setUpClass()
+        self.user=Client()
+        self.client_admin = Client()
+        self.dataset_url=reverse("dataset/v2-list")
+        self.dataset_files_url=reverse("dataset_files-list")
+        self.dashboard_url = reverse("new_dashboard-dashboard")
+        # print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        # print(self.dashboard_url)
+        self.admin_role = UserRole.objects.create(
+                        id= "1",
+                        role_name= "datahub_admin"
+                        )
+        self.admin_user1 = User.objects.create(
+                        email= "admin1@dgreen.org",
+                        role_id= self.admin_role.id,
+                        )
+        self.admin_org1 = Organization.objects.create(
+                        org_email= "admin1_org@dg.org",
+                        name= "admin org1",
+                        phone_number= "+91 99876-62188",
+                        website= "htttps://google.com",
+                        address= ({"city": "Banglore"}),
+                        )
+        self.admin_map1 = UserOrganizationMap.objects.create(
+                                        user_id= self.admin_user1.id,
+                                        organization_id= self.admin_org1.id,
+                                        )
+        auth["token"] = TestUtils.create_token_for_user(self.admin_user1, self.admin_map1)
+        admin_header= self.set_auth_headers(self=self)
+        self.client_admin.defaults['HTTP_AUTHORIZATION'] = admin_header[0]
+        self.client_admin.defaults['CONTENT_TYPE'] = admin_header[1]
+        self.first_dataset = DatasetV2.objects.create( user_map=self.admin_map1,
+                                                **first_datasets)
+        self.dataset_id1 = self.first_dataset.id
+        self.second_dataset = DatasetV2.objects.create( user_map=self.admin_map1,
+                                                **second_datasets)
+        self.dataset_id2 = self.second_dataset.id
+        self.third_dataset = DatasetV2.objects.create( user_map=self.admin_map1,
+                                                **third_datasets)
+        self.dataset_id3 = self.third_dataset.id
+        file_data1 = {
+            "dataset": str(self.first_dataset.id),
+            "file" : open("datahub/tests/test_data/file.xls", "rb"),
+            "source": "file",
         }
-        response = self.client_admin.post(self.dataset_request+"requested_datasets/", data_user)
+        self.response_file1 = self.client_admin.post(self.dataset_files_url, file_data1)
+        file_data2 = {
+            "dataset": str(self.first_dataset.id),
+            "file" : open("datahub/tests/test_data/file_example_XLS_10.xls", "rb"),
+            "source": "file",
+            "accessibility": "private",
+        }
+        self.response_file2 = self.client_admin.post(self.dataset_files_url, file_data2)
+        # print("hi--->",self.response_file1)
+        # print("hifdfehfrfurfrjjg5rkj")
+        connector_cs = Connectors.objects.create(user_id=self.admin_user1.id,
+                                                 **connectors_info)
+        connector_map_cs = ConnectorsMap.objects.create(connectors_id=connector_cs.id,
+                            left_dataset_file_id = self.response_file1.json()['id'],
+                            right_dataset_file_id = self.response_file2.json()['id'])
+        
+        print("*** connector_map_cs ***", connector_map_cs.id)
+     
+    # @classmethod
+    def set_auth_headers(self, participant=False, co_steward=False):  
+        """ authorization """
+        auth_data = auth_participant if participant else (auth_co_steward if co_steward else auth)
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {auth_data["token"]}'
+        }
+        return headers['Authorization'],headers['Content-Type']
+    
+    #test case for admin my_org for dashboard
+    def test_dashboard_admin_valid_(self):
+        """ retrieving self dataset details by admin1 user"""
+        # https://datahubethdev.farmstack.co/be/datahub/newdashboard/dashboard/?my_org=True
+        params= "?my_org=True/"
+        data = {
+            "my_org": True
+        }
+        response = self.client_admin.get(self.dashboard_url+params)
         data = response.json()
-        # print("***test_request_dataset_invalid***", response.status_code)
-        # print("***test_request_dataset_invalid***", data)
-        assert response.status_code == 500
-        assert data == f"Issue while Retrive requeted data ['“{data_user['user_map']}” is not a valid UUID.']"
+        print("***test_dashboard_admin_valid_***", data)
+        print("***test_dashboard_admin_valid_***", response.status_code)
+        # assert response.status_code == 200
+        # assert data['name'] == first_datasets_dump_data['name']
+        # assert data['description'] == first_datasets_dump_data['description']
+       
+    
+    
+    
