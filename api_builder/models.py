52949c7a880f057735255ba9863ae37f9c214151
photon0205 <sp.code2003@gmail.com>
2023-08-28 12:00:00 +0100

    docs: add documentation

--- api_builder/models.py
+++ api_builder/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 from datahub.models import DatasetV2File
 
+
 class API(models.Model):
-    dataset_file = models.ForeignKey(DatasetV2File, on_delete=models.CASCADE, related_name='apis')
-    endpoint = models.CharField(max_length=100)
+    """
+    API Model - Represents an API configuration.
+
+    Attributes:
+        - dataset_file: ForeignKey to the associated DatasetV2File.
+        - endpoint: The API endpoint (unique).
+        - selected_columns: JSONField for storing selected columns.
+        - access_key: API access key.
+    """
+
+    dataset_file = models.ForeignKey(DatasetV2File, on_delete=models.CASCADE, related_name="apis")
+    endpoint = models.CharField(max_length=100, unique=True)
     selected_columns = models.JSONField()
-    access_key = models.CharField(max_length=100, blank=True, null=True)
+    access_key = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+        Returns a string representation of the API instance.
+        """
         return self.endpoint
--- api_builder/utils.py
+++ api_builder/utils.py
@@ -5,27 +5,48 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from .models import API
 
-class APIKeyAuthentication(BaseAuthentication):
-    def authenticate(self, request):
-        api_key = request.headers.get('Authorization', '')
 
-        if not api_key.strip() or api_key.lower() == 'none':
-            raise AuthenticationFailed('No API key provided.')
+class APIKeyAuthentication(BaseAuthentication):
+    """
+    Custom authentication class for API key authentication.
+    """
+
+    def authenticate(self, request):
+        api_key = request.headers.get("Authorization", "")
+
+        if not api_key.strip() or api_key.lower() == "none":
+            raise AuthenticationFailed("No API key provided.")
 
         api_key = api_key.split()[-1]
 
         try:
             api = API.objects.get(access_key=api_key)
         except API.DoesNotExist:
-            raise AuthenticationFailed('API key is invalid.')
+            raise AuthenticationFailed("API key is invalid.")
 
         return (api, None)
 
+
 def generate_api_key():
+    """
+    Generate a random API access key.
+
+    Returns:
+        A randomly generated API access key.
+    """
     return str(uuid.uuid4())
 
+
 def read_columns_from_csv_or_xlsx_file(file_path):
-    """This function reads the file and returns the column names as a list."""
+    """
+    Read column names from a CSV or XLSX file.
+
+    Args:
+        file_path: The path to the file.
+
+    Returns:
+        A list of column names.
+    """
     try:
         if file_path.endswith(".xlsx") or file_path.endswith(".xls"):
             content = pd.read_excel(file_path, index_col=None, nrows=21).head(2) if file_path else None
@@ -33,7 +54,7 @@ def read_columns_from_csv_or_xlsx_file(file_path):
             content = pd.read_csv(file_path, index_col=False, nrows=21).head(2) if file_path else None
 
         if content is not None:
-            content = content.drop(content.filter(regex='Unnamed').columns, axis=1)
+            content = content.drop(content.filter(regex="Unnamed").columns, axis=1)
             content = content.fillna("")
             column_names = content.columns.astype(str).tolist()
         else:
