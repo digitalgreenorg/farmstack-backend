9155f7b5748ccdd72d2417873e84af16b81d6911
ugeshdg <ugesh@digitalgreen.org>
2023-09-08 11:06:45 +0300

    dashboard updated

--- microsite/views.py
+++ microsite/views.py
@@ -514,8 +514,6 @@ class DatasetsMicrositeViewSet(GenericViewSet):
                     f"Something went wrong, please try again. {e}",
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-            cache.set(hash_key, data, 86400)
-            LOGGER.info("Dashboard details added to cache", exc_info=True)
             return Response(
                 data,
                 status=status.HTTP_200_OK,
