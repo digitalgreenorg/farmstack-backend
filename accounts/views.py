492f029e0b14359b2464498fda136164e125b18b
ugeshdg <ugesh@digitalgreen.org>
2024-02-02 21:40:02 +0530

    prompt updated

--- accounts/views.py
+++ accounts/views.py
@@ -201,7 +201,17 @@ class LoginViewset(GenericViewSet):
 
             email_render = render(request, "otp.html", data)
             mail_body = email_render.content.decode("utf-8")
+            print(cache.get(email))
+            login_helper.set_user_otp(email, otp, settings.OTP_DURATION)
 
+            return Response(
+                {
+                    "id": user.id,
+                    "email": email,
+                    "message": "Enter the OTP to login",
+                },
+                status=status.HTTP_201_CREATED,
+            )
             Utils().send_email(
                 to_email=email,
                 # content=f"Your OTP is {otp}",
