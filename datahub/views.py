0ee27c7e393ccc29f7ba7dc624d8524a7e4316c2
ugeshdg <ugesh@digitalgreen.org>
2025-01-27 11:36:43 +0530

    celery updated

--- datahub/views.py
+++ datahub/views.py
@@ -3569,7 +3569,7 @@ class ResourceFileManagementViewSet(GenericViewSet):
                 serializer_data["states"] = states
                 serializer_data["districts"] = districts
 
-                create_vector_db(serializer_data)
+                create_vector_db.delay(serializer_data)
                 # create_vector_db(serializer_data)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             else:
