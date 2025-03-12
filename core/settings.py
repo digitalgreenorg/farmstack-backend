e8fc0ea95ae14e38c959a1da125ebb26e785cd64
ugeshdg <ugesh@digitalgreen.org>
2025-03-12 12:09:18 +0530

    scheduler updated to 1hr

--- core/settings.py
+++ core/settings.py
@@ -461,7 +461,7 @@ CELERY_TIMEZONE = 'UTC'
 CELERY_BEAT_SCHEDULE = {
     'fetch_dataset_for_all_files': {
         'task': 'core.utils.fetch_data_for_all_datasets',
-        'schedule': crontab(minute=0, hour=0),  # Daily at midnight
+        'schedule': crontab(minute=0, hour='*/1'),  # Daily at midnight minite=0 hour=0
         #  'schedule': crontab(minute='*/1')
     },
 }
