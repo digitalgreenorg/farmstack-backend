7e6f12ffd6342b119df9b42350fa29cf519cb498
ugeshdg <96171777+ugeshdg@users.noreply.github.com>
2025-06-03 11:49:35 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -354,6 +354,19 @@ class LoginViewset(GenericViewSet):
             )
 
 
+     def bot_login(self, request, *args, **kwargs):
+        """POST method: to save a newly registered user"""
+        email=request.data.get("email")
+        user_obj = User.objects.filter(email=email)
+        user = user_obj.first()
+        if not user:
+            return Response(
+                {"email": "User not registered"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        serializer = UserCreateSerializer(user)
+        return Response(serializer.data)
+         
 @permission_classes([])
 class ResendOTPViewset(GenericViewSet):
     """ResendOTPViewset for users to register"""
